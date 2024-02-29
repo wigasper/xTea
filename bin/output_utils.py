ae06d768badce5870af157058d76083672dab4f5
William Gasper <wkg@williamgasper.com>
2024-02-29 08:02:36 -0600

    logic for writing serial and gnu parallel shellscript output

---
+++ bin/output_utils.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from typing import Never, List
+
+
+def write_serial_output(output_fp: Path, shell_script_fps: List[Path]) -> Never:
+    with open(output_fp, "w") as out:
+        out.write("#!/bin/bash\n\n")
+
+        for fp in shell_script_fps:
+            out.write(f"bash {fp}\n")
+
+
+def write_gnu_parallel_output(
+    output_fp: Path, shell_script_fps: List[Path], num_threads: int
+) -> Never:
+    with open(output_fp, "w") as out:
+        out.write("#!/bin/bash\n\n")
+
+        array_elements = "\n".join([f'"{fp}"' for fp in shell_script_fps])
+
+        out.write("# Array of shell script file paths\n")
+        out.write(f"shell_script_fps=({array_elements})\n\n")
+
+        out.write(
+            f'printf "%s\n" "${{script_paths[@]}}" | parallel -j {num_threads} bash {{}}'
+        )
